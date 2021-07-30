154358685b0c85e8ff09619a5c67d725de50987c
Tanner-Wills <tannerwills1@gmail.com>
2021-07-30 08:46:00 -0400

    Initial commit

---
+++ src/sample/tempConvert.java
@@ -0,0 +1,24 @@
+package sample;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import  javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+public class tempConvert extends Application {
+
+    public void start(Stage stage){
+        Button convertButton = new Button();
+
+
+
+    }
+
+
+
+
+}
